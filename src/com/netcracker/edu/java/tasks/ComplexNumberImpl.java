1f2be28cc9f6ea50f9615c720e6ab81c1ea5d157
Andrey Shagov <writeletterto@me.com>
2018-03-21 12:00:46 +0300

    Initial commit add interface

---
+++ src/com/netcracker/edu/java/tasks/ComplexNumberImpl.java
@@ -0,0 +1,63 @@
+package com.netcracker.edu.java.tasks;
+
+public class ComplexNumberImpl implements ComplexNumber {
+    @Override
+    public double getRe() {
+        return 0;
+    }
+
+    @Override
+    public double getIm() {
+        return 0;
+    }
+
+    @Override
+    public boolean isReal() {
+        return false;
+    }
+
+    @Override
+    public void set(double re, double im) {
+
+    }
+
+    @Override
+    public void set(String value) throws NumberFormatException {
+
+    }
+
+    @Override
+    public ComplexNumber copy() {
+        return null;
+    }
+
+    @Override
+    public ComplexNumber clone() throws CloneNotSupportedException {
+        return null;
+    }
+
+    @Override
+    public int compareTo(ComplexNumber other) {
+        return 0;
+    }
+
+    @Override
+    public void sort(ComplexNumber[] array) {
+
+    }
+
+    @Override
+    public ComplexNumber negate() {
+        return null;
+    }
+
+    @Override
+    public ComplexNumber add(ComplexNumber arg2) {
+        return null;
+    }
+
+    @Override
+    public ComplexNumber multiply(ComplexNumber arg2) {
+        return null;
+    }
+}
